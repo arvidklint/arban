dda010f30ba5a5fe4af7a9ec17df62ac02f7b52b
Anton Sivertsson <sivertsson.anton@gmail.com>
2015-12-17 16:33:51 +0100

    Add support for selection and rotation of 3D objects

---
+++ arban/Assets/Scripts/Item.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(MeshCollider))]
+
+public class Item : MonoBehaviour {
+	private Vector3 screenPoint;
+	private Vector3 offset;
+	MasterController mc;
+
+	void Start() {
+		mc = transform.parent.GetComponent<MasterController>();
+		mc.addObserver(this);
+	}
+	
+	void OnMouseDown() {
+		mc.select(gameObject);
+		screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+		offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+	}
+	
+	void OnMouseDrag() {
+		if (!(Input.touchCount == 2)) {
+			Vector3 curScreenPoint = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+			Vector3 curPosition = Camera.main.ScreenToWorldPoint (curScreenPoint) + offset;
+			transform.position = curPosition;
+		}
+	}
+
+	public void updateSelf(string msg) {
+		if (msg == "select") {
+			if (gameObject == mc.selected) {
+				select ();
+			} else {
+				deselect ();
+			}
+		}
+	}
+
+	void select() {
+		setItemSelectionColor(Color.blue);
+	}
+
+	void deselect() {
+		setItemSelectionColor(Color.white);
+	}
+
+	void setItemSelectionColor(Color c) {
+		Renderer rend = GetComponent<Renderer>();
+		rend.material.SetColor ("_Color", c);
+	}
+}
---
+++ arban/Assets/Scripts/MasterController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MasterController : MonoBehaviour {
+
+	public GameObject selected;
+	public ArrayList items = new ArrayList();
+
+	public void select(GameObject item) {
+		selected = item;
+		Debug.Log ("Nu happens grejer");
+		notifyObservers("select");
+	}
+
+	public void addObserver(Item item) {
+		items.Add(item);
+	}
+
+	public void notifyObservers(string msg) {
+		Debug.Log (items);
+
+		foreach (Item item in items) {
+			item.updateSelf(msg);
+		}
+	}
+
+	void LateUpdate() {
+		float pinchAmount = 0;
+		Quaternion desiredRotation = selected.transform.rotation;
+		
+		DetectTouchMovement.Calculate();
+		
+		if (Mathf.Abs(DetectTouchMovement.turnAngleDelta) > 0) { // rotate
+			Debug.Log("Is rotating");
+			Vector3 rotationDeg = Vector3.zero;
+			rotationDeg.z = DetectTouchMovement.turnAngleDelta;
+			desiredRotation *= Quaternion.Euler(rotationDeg*2);
+		}
+		
+		// not so sure those will work:
+		selected.transform.rotation = desiredRotation;
+		selected.transform.position += Vector3.forward * pinchAmount;
+	}
+}
