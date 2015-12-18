2cc3ad6b67497138d929dc261ad97729abe84baf
Rasmus Berggrén <rajje@mac.se>
2015-12-18 14:27:14 +0100

    Add UI. Structure selection system. Modularise Item and MasterController methods.

--- arban/Assets/Scripts/Item.cs
+++ arban/Assets/Scripts/Item.cs
@@ -7,20 +7,28 @@ public class Item : MonoBehaviour {
 	private Vector3 screenPoint;
 	private Vector3 offset;
 	MasterController mc;
+	Vector2 mouseDownPos;
+	bool mouseHasMoved;
 
 	void Start() {
 		mc = transform.parent.GetComponent<MasterController>();
 		mc.addObserver(this);
+		Debug.Log ("Ett objekt är skapat "  + GetInstanceID());
 	}
 	
 	void OnMouseDown() {
-		mc.select(gameObject);
+		mouseDownPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
 		screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
 		offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
 	}
+
+	void OnMouseUp() {
+		mouseHasMoved = mc.compareMousePositions(mouseDownPos, new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+		if (!mouseHasMoved) mc.toggleSelect(gameObject);
+	}
 	
 	void OnMouseDrag() {
-		if (!(Input.touchCount == 2)) {
+		if (!(Input.touchCount == 2) && isSelected()) {
 			Vector3 curScreenPoint = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
 			Vector3 curPosition = Camera.main.ScreenToWorldPoint (curScreenPoint) + offset;
 			transform.position = curPosition;
@@ -29,22 +37,30 @@ public class Item : MonoBehaviour {
 
 	public void updateSelf(string msg) {
 		if (msg == "select") {
-			if (gameObject == mc.selected) {
-				select ();
-			} else {
-				deselect ();
-			}
+			updateSelectionStatus();
 		}
 	}
 
-	void select() {
+	public bool isSelected() {
+		return (gameObject == mc.selected);
+	}
+
+	void updateSelectionStatus() {
+		if (isSelected()) {
+			markSelected();
+		} else {
+			markDeselected();
+		}
+	}
+
+	void markSelected() {
 		setItemSelectionColor(Color.blue);
 	}
 
-	void deselect() {
+	void markDeselected() {
 		setItemSelectionColor(Color.white);
 	}
-
+	
 	void setItemSelectionColor(Color c) {
 		Renderer rend = GetComponent<Renderer>();
 		rend.material.SetColor ("_Color", c);
--- arban/Assets/Scripts/MasterController.cs
+++ arban/Assets/Scripts/MasterController.cs
@@ -7,9 +7,41 @@ public class MasterController : MonoBehaviour {
 	public GameObject selected;
 	public ArrayList items = new ArrayList();
 
+	public void DeleteSelected() {
+//		Debug.Log ("Objekt i Items: ");
+//		foreach (Item item in items) {
+//			Debug.Log (item.GetInstanceID());
+//		}
+//		Debug.Log ("Tar bort: " + selected.GetInstanceID());
+//		items.Remove(selected);
+//		Debug.Log ("Objekt i Items: ");
+//		foreach (Item item in items) {
+//			Debug.Log (item.GetInstanceID());
+//		}
+//		Destroy (selected);
+//		Debug.Log ("DeleteSelected");
+	}
+
+	public void addObserver2(Item item) {
+		items.Add(item);
+	}
+
+	public void toggleSelect(GameObject item) {
+		if (item == selected) deselect(item); else select(item);
+	}
+
 	public void select(GameObject item) {
 		selected = item;
-		Debug.Log ("Nu happens grejer");
+		notifyObservers("select");
+	}
+
+	public void deselect(GameObject item) {
+		// Ska implementeras i framtiden när enstaka objekt kan avmarkeras. Tills vidare kör den DeselectAll. 
+		DeselectAll();
+	}
+
+	public void DeselectAll() {
+		selected = null;
 		notifyObservers("select");
 	}
 
@@ -18,28 +50,36 @@ public class MasterController : MonoBehaviour {
 	}
 
 	public void notifyObservers(string msg) {
-		Debug.Log (items);
-
 		foreach (Item item in items) {
 			item.updateSelf(msg);
 		}
 	}
 
+	public bool somethingSelected() {
+		return (selected != null);
+	}
+
+	public bool compareMousePositions(Vector2 pos1, Vector2 pos2) {
+		return ((pos1[0] == pos2[0]) && (pos1[1] == pos2[1])) ? false : true;
+	}
+	
 	void LateUpdate() {
-		float pinchAmount = 0;
-		Quaternion desiredRotation = selected.transform.rotation;
-		
-		DetectTouchMovement.Calculate();
-		
-		if (Mathf.Abs(DetectTouchMovement.turnAngleDelta) > 0) { // rotate
-			Debug.Log("Is rotating");
-			Vector3 rotationDeg = Vector3.zero;
-			rotationDeg.z = DetectTouchMovement.turnAngleDelta;
-			desiredRotation *= Quaternion.Euler(rotationDeg*2);
+		if (somethingSelected()) {
+			float pinchAmount = 0;
+			Quaternion desiredRotation = selected.transform.rotation;
+			
+			DetectTouchMovement.Calculate();
+			
+			if (Mathf.Abs(DetectTouchMovement.turnAngleDelta) > 0) { // rotate
+				Debug.Log("Is rotating");
+				Vector3 rotationDeg = Vector3.zero;
+				rotationDeg.z = DetectTouchMovement.turnAngleDelta;
+				desiredRotation *= Quaternion.Euler(rotationDeg*2);
+			}
+			
+			// not so sure those will work:
+			selected.transform.rotation = desiredRotation;
+			selected.transform.position += Vector3.forward * pinchAmount;
 		}
-		
-		// not so sure those will work:
-		selected.transform.rotation = desiredRotation;
-		selected.transform.position += Vector3.forward * pinchAmount;
 	}
 }
